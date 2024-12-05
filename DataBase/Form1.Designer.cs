3bbd73d1cd723ed8a55d875e1e62f1678f8b4428
Адми <@DESKTOP-FJVRIJS>
2024-12-06 00:42:47 +0300

    Добавьте файлы проекта.

---
+++ DataBase/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace DataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.dataGridView6 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1127, 585);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(436, 224);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(454, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(436, 224);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(896, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(436, 224);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(12, 323);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(436, 224);
+            this.dataGridView4.TabIndex = 4;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(454, 323);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(436, 224);
+            this.dataGridView5.TabIndex = 5;
+            // 
+            // dataGridView6
+            // 
+            this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView6.Location = new System.Drawing.Point(896, 323);
+            this.dataGridView6.Name = "dataGridView6";
+            this.dataGridView6.Size = new System.Drawing.Size(436, 224);
+            this.dataGridView6.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1249, 585);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 59);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "clients",
+            "deals",
+            "legaldocuments",
+            "payments",
+            "properties",
+            "requestes"});
+            this.comboBox1.Location = new System.Drawing.Point(961, 605);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 656);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView6);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.DataGridView dataGridView6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ DataBase/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace DataBase
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, DataGridView> tableViews;
+        private string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=2603;Database=Agent";
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTableViews();
+        }
+
+        private void InitializeTableViews()
+        {
+            tableViews = new Dictionary<string, DataGridView>
+            {
+                { "clients", dataGridView1 },
+                { "properties", dataGridView2 },
+                { "deals", dataGridView3 },
+                { "payments", dataGridView4 },
+                { "legaldocuments", dataGridView5 },
+                { "requests", dataGridView6 }
+            };
+            
+        }
+
+        
+        private void YourMethod()
+        {
+            
+
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Подключение к базе данных успешно установлено!");
+
+                    //
+                    string clients = "SELECT * FROM clients";
+                    using (var command = new NpgsqlCommand(clients, connection))
+                    {
+                        using (var adapter = new NpgsqlDataAdapter(command))
+                        {
+                            DataTable dataTableJobPostings = new DataTable();
+                            adapter.Fill(dataTableJobPostings);
+                            dataGridView1.DataSource = dataTableJobPostings;
+                        }
+                    }
+
+                    //
+                    string deals = "SELECT * FROM deals";
+                    using (var command = new NpgsqlCommand(deals, connection))
+                    {
+                        using (var adapter = new NpgsqlDataAdapter(command))
+                        {
+                            DataTable dataTableJobPostings = new DataTable();
+                            adapter.Fill(dataTableJobPostings);
+                            dataGridView2.DataSource = dataTableJobPostings;
+                        }
+                    }
+
+                    //
+                    string legaldocuments = "SELECT * FROM legaldocuments";
+                    using (var command = new NpgsqlCommand(legaldocuments, connection))
+                    {
+                        using (var adapter = new NpgsqlDataAdapter(command))
+                        {
+                            DataTable dataTableJobPostings = new DataTable();
+                            adapter.Fill(dataTableJobPostings);
+                            dataGridView3.DataSource = dataTableJobPostings;
+                        }
+                    }
+
+                    //
+                    string payments = "SELECT * FROM payments";
+                    using (var command = new NpgsqlCommand(payments, connection))
+                    {
+                        using (var adapter = new NpgsqlDataAdapter(command))
+                        {
+                            DataTable dataTableJobPostings = new DataTable();
+                            adapter.Fill(dataTableJobPostings);
+                            dataGridView4.DataSource = dataTableJobPostings;
+                        }
+                    }
+
+                    //
+                    string properties = "SELECT * FROM properties";
+                    using (var command = new NpgsqlCommand(properties, connection))
+                    {
+                        using (var adapter = new NpgsqlDataAdapter(command))
+                        {
+                            DataTable dataTableJobPostings = new DataTable();
+                            adapter.Fill(dataTableJobPostings);
+                            dataGridView5.DataSource = dataTableJobPostings;
+                        }
+                    }
+
+                    //
+                    string requestes = "SELECT * FROM requestes";
+                    using (var command = new NpgsqlCommand(requestes, connection))
+                    {
+                        using (var adapter = new NpgsqlDataAdapter(command))
+                        {
+                            DataTable dataTableJobPostings = new DataTable();
+                            adapter.Fill(dataTableJobPostings);
+                            dataGridView6.DataSource = dataTableJobPostings;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка подключения: {ex.Message}");
+                }
+            }
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string selectedTable = comboBox1.SelectedItem.ToString();
+                if (tableViews.ContainsKey(selectedTable))
+                {
+                    var dataGridView = tableViews[selectedTable];
+
+                    using (var connection = new NpgsqlConnection(connectionString))
+                    {
+                        try
+                        {
+                            connection.Open();
+
+                            
+                            var dataTable = (DataTable)dataGridView.DataSource;
+                            var newRow = dataTable.NewRow(); 
+                            dataTable.Rows.Add(newRow);
+
+                            
+                            var adapter = new NpgsqlDataAdapter($"SELECT * FROM {selectedTable}", connection);
+                            var commandBuilder = new NpgsqlCommandBuilder(adapter);
+                            adapter.Update(dataTable);
+
+                            MessageBox.Show($"Строка добавлена в таблицу {selectedTable}.");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка добавления строки в таблицу {selectedTable}: {ex.Message}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите таблицу из списка.");
+            }
+            YourMethod();
+        }
+            
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            YourMethod();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string selectedTable = comboBox1.SelectedItem.ToString();
+                if (tableViews.ContainsKey(selectedTable))
+                {
+                    var dataGridView = tableViews[selectedTable];
+
+                    if (dataGridView.SelectedRows.Count > 0)
+                    {
+                        using (var connection = new NpgsqlConnection(connectionString))
+                        {
+                            try
+                            {
+                                connection.Open();
+
+                                
+                                foreach (DataGridViewRow row in dataGridView.SelectedRows)
+                                {
+                                    if (!row.IsNewRow)
+                                    {
+                                        dataGridView.Rows.Remove(row);
+                                    }
+                                }
+
+                                
+                                var dataTable = (DataTable)dataGridView.DataSource;
+                                var adapter = new NpgsqlDataAdapter($"SELECT * FROM {selectedTable}", connection);
+                                var commandBuilder = new NpgsqlCommandBuilder(adapter);
+                                adapter.Update(dataTable);
+
+                                MessageBox.Show($"Выбранные строки удалены из таблицы {selectedTable}.");
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show($"Ошибка удаления строки из таблицы {selectedTable}: {ex.Message}");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выберите строку для удаления.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите таблицу из списка.");
+            }
+            YourMethod();
+        }
+            
+    }
+}
